e197bb53e4c3f377ce78a51b4b91ccffa568455a
Aniket Tiwari <ani962854.at@gmail.com>
2024-10-24 11:02:29 +0530

    refactored main activity and added camera permission check

--- app/src/main/java/com/anikettcodes/gesturefy/presentation/activities/MainActivity.kt
+++ app/src/main/java/com/anikettcodes/gesturefy/presentation/activities/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.anikettcodes.gesturefy.presentation.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
@@ -14,9 +18,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.core.content.ContextCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.rememberNavController
 import com.anikettcodes.gesturefy.BuildConfig
@@ -28,7 +34,6 @@ import com.anikettcodes.gesturefy.presentation.viewmodels.HomeViewmodel
 import com.spotify.android.appremote.api.ConnectionParams
 import com.spotify.android.appremote.api.Connector
 import com.spotify.android.appremote.api.SpotifyAppRemote
-import com.spotify.protocol.types.Track
 import com.spotify.sdk.android.auth.AuthorizationClient
 import com.spotify.sdk.android.auth.AuthorizationRequest
 import com.spotify.sdk.android.auth.AuthorizationResponse
@@ -36,14 +41,34 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
     private var spotifyAppRemote:SpotifyAppRemote? = null
+    private val permissionCheckActivityResult =  registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+
+       if(!isGranted){
+           Toast.makeText(this,"Camera permission denied!",Toast.LENGTH_LONG)
+       }
+
+
+    }
+
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val data: Uri? = intent?.data
-        data?.let {
-            Log.d(TAG,it.toString())
+        var data: Uri? = intent?.data
+
+        when(PackageManager.PERMISSION_GRANTED){
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.CAMERA
+            ) -> {
+
+            }
+            else -> {
+                permissionCheckActivityResult.launch(Manifest.permission.CAMERA)
+            }
         }
+
         setContent {
             GestureFyTheme {
                 // A surface container using the 'background' color from the theme
@@ -51,97 +76,58 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val homeViewmodel = hiltViewModel<HomeViewmodel>()
-                    root(
-                        data = data,
-                        homeViewmodel = homeViewmodel,
-                        onNext = {
-                            spotifyAppRemote?.playerApi?.skipNext()
-                        },
-                        onPrev = {
-                            spotifyAppRemote?.playerApi?.skipPrevious()
-                        },
-                        onLogIn = {
-                            val connectionParam = ConnectionParams.Builder(BuildConfig.CLIENT_ID)
-                                .setRedirectUri(BuildConfig.REDIRECT_URI)
-                                .build()
-
-                            SpotifyAppRemote.connect(this,connectionParam, object : Connector.ConnectionListener{
-                                override fun onConnected(p0: SpotifyAppRemote?) {
-                                    spotifyAppRemote = p0
-                                    connected(homeViewmodel)
-                                }
-
-                                override fun onFailure(p0: Throwable?) {
-                                    Log.e(TAG,p0?.message?:"Could not connect to spotify")
-                                }
-                            })
-                        }
-                    ){
-                        val authorizationBuilder = AuthorizationRequest.Builder(BuildConfig.CLIENT_ID,AuthorizationResponse.Type.CODE,BuildConfig.REDIRECT_URI)
-                        authorizationBuilder.setScopes(
-                            arrayOf("app-remote-control")
-                        )
-
-                        AuthorizationClient.openLoginInBrowser(this,authorizationBuilder.build())
-
+                    val authorizationViewmodel = hiltViewModel<AuthorizationViewmodel>()
+                    if(data != null ){
+                        authorizationViewmodel.authorizeUser(data!!)
+                       data = null
                     }
+
+                    if(authorizationViewmodel.state.value.isLoading) {
+                        Text(text = "Loading")
+                    }
+                    else if(authorizationViewmodel.state.value.isLoggedIn){
+
+                        val homeViewmodel = hiltViewModel<HomeViewmodel>()
+                        //setup spotify remote
+                        initializeSpotifyAppRemote(homeViewmodel)
+                        HomeScreen(homeState = homeViewmodel.state.value, spotifyAppRemote = spotifyAppRemote)
+                    }
+                    else   {
+                        AuthorizationScreen {
+                            val authorizationBuilder = AuthorizationRequest.Builder(BuildConfig.CLIENT_ID,AuthorizationResponse.Type.CODE,BuildConfig.REDIRECT_URI)
+                            authorizationBuilder.setScopes(
+                                arrayOf("app-remote-control")
+                            )
+                            AuthorizationClient.openLoginInBrowser(this,authorizationBuilder.build())
+                        }
+                    }
+
                 }
             }
         }
     }
 
-    private fun connected(viewmodel: HomeViewmodel) {
-        spotifyAppRemote?.let {
-            // Play a playlist
-            val playlistURI = "spotify:playlist:37i9dQZF1DX2sUQwD7tbmL"
-            // Subscribe to PlayerState
-            it.playerApi.subscribeToPlayerState().setEventCallback {
-                viewmodel.updateTrackState(it.track.name,it.track.artist.name)
-            }
-        }
 
+    private fun initializeSpotifyAppRemote(viewmodel: HomeViewmodel){
+        SpotifyAppRemote.connect(this, connectionParams,object:Connector.ConnectionListener {
+            override fun onConnected(p0: SpotifyAppRemote?) {
+                spotifyAppRemote = p0
+                spotifyAppRemote?.playerApi?.subscribeToPlayerState()?.setEventCallback {
+                    viewmodel.updateTrackState(it.track.name,it.track.artist.name)
+                }
+            }
+
+            override fun onFailure(p0: Throwable?) {
+                Log.e(TAG,p0?.message?:"Unknown error while setting up Spotify app remote")
+            }
+
+        })
     }
+
     companion object{
         const val TAG = "MAIN_ACTIVITY"
-    }
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Composable
-fun root(
-    data:Uri?,
-    homeViewmodel: HomeViewmodel,
-    onNext:()->Unit,
-    onPrev:()->Unit,
-    onLogIn:()->Unit,
-    connectSpotify:()->Unit,
-){
-    var authorizationCalled by rememberSaveable{
-        mutableStateOf(false)
-    }
-    val navController = rememberNavController()
-    val authorizationViewModel = hiltViewModel<AuthorizationViewmodel>()
-    if(data != null && !authorizationCalled){
-        authorizationViewModel.authorizeUser(data)
-        authorizationCalled = true
-    }
-    if(authorizationViewModel.state.value.isLoading){
-        Text(text = "Loading...")
-    }
-    else if(authorizationViewModel.state.value.isLoggedIn){
-        onLogIn()
-
-        HomeScreen(
-            trackName = homeViewmodel.state.value.trackName,
-            artistName = homeViewmodel.state.value.artistName,
-            onNext = onNext,
-            onPrev = onPrev
-        )
-    }
-    else{ 
-        AuthorizationScreen {
-            connectSpotify()
-        }
+        val connectionParams: ConnectionParams = ConnectionParams.Builder(BuildConfig.CLIENT_ID)
+            .setRedirectUri(BuildConfig.REDIRECT_URI)
+            .build()
     }
 }
